587192fcc53d70e5d3aefe12bc73cb23d616bfc0
Matthieu Brossault <matthieu.brossault@gmail.com>
2022-03-25 10:48:26 +0100

    For add-task form use formGroup and validators

--- src/app/components/task-add/task-add.component.ts
+++ src/app/components/task-add/task-add.component.ts
@@ -1,51 +1,42 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { Task } from '../../Task';
-import { UiService } from 'src/app/services/ui.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
 
 @Component({
   selector: 'app-task-add',
   templateUrl: './task-add.component.html',
   styleUrls: ['./task-add.component.css']
 })
-export class TaskAddComponent implements OnInit {
-  text: string | undefined;
-  day: string | undefined;
-  reminder: boolean = false;
+export class TaskAddComponent {
   showAddForm: boolean = false;
   subscription: Subscription = new Subscription();
 
   @Output() onAddEvent: EventEmitter<Task> = new EventEmitter();
 
-  constructor(private uiService: UiService) {
+  addTaskForm: FormGroup = this.formBuilder.group({
+    text: ["", Validators.required],
+    day: ["", Validators.required],
+    reminder: false
+  });
+
+  constructor(private uiService: UiService, private formBuilder: FormBuilder) {
     this.subscription = this.uiService.onToggle().subscribe((val) => this.showAddForm = val)
   }
 
-  ngOnInit(): void {
+  get text() {
+    return this.addTaskForm.get("text");
   }
 
-  onSubmit() {
-    if (!this.text) {
-      alert('Please add a label');
-      return;
-    }
+  get day() {
+    return this.addTaskForm.get("day");
+  }
 
-    if (!this.day) {
-      alert('Please add a Date & Time');
-      return;
-    }
+  onSubmit(): void {
+    this.onAddEvent.emit(this.addTaskForm.value);
 
-    const newTask: Task = {
-      text: this.text,
-      day: this.day,
-      reminder: this.reminder
-    }
-
-    this.onAddEvent.emit(newTask);
-
-    this.text = '';
-    this.day = '';
-    this.reminder = false;
+    this.addTaskForm.reset();
   }
 
 }
